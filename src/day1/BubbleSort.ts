d474cd56e200b58f49e294add4910de65e05f747
IbrahimAlshubaily <ibra.94@live.com>
2022-09-20 08:49:23 +0300

    bubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    //bubble max to length - i - 1
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,9 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
-
+    if (a != b) {
+        return false;
+    }
+    if (!a || !b) {
+        return true;
+    }
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
