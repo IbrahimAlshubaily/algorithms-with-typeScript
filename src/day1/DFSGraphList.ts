8c55cabd74558a93062fae2034feb68289ac8d85
IbrahimAlshubaily <ibra.94@live.com>
2022-09-20 09:07:59 +0300

    DFS on Graph and BST

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,34 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
 
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen : boolean[] = Array(graph.length).fill(false);
+    const path : number[] = [];
+    seen[source] = true;
+    walk(graph, source, needle, seen, path)
+    if (path.length === 0) {
+        return null;
+    }
+    return path;
+}
+
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen : boolean[], path : number[]): boolean {
+    
+    path.push(curr);
+    if (curr === needle) {
+        return true;
+    }
+
+    const adj = graph[curr];
+    for (let i = 0; i < adj.length; i++) {
+
+        if (!seen[adj[i].to]){
+
+            seen[adj[i].to] = true;
+            if (walk(graph, adj[i].to, needle, seen, path)){
+                return true;
+            }
+        }
+    }
+
+    path.pop();
+    return false;
 }
