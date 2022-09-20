4bfbb8d3992874a0b3eef350b521dac039f99bd3
IbrahimAlshubaily <ibra.94@live.com>
2022-09-20 08:21:14 +0300

    Binary and Linear Search - Lists

--- src/__tests__/BinarySearchList.ts
+++ src/__tests__/BinarySearchList.ts
@@ -3,6 +3,7 @@ import binary_fn from "@code/BinarySearchList"
 test("binary search array", function() {
 
     const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+    debugger;
     expect(binary_fn(foo, 69)).toEqual(true);
     expect(binary_fn(foo, 1336)).toEqual(false);
     expect(binary_fn(foo, 69420)).toEqual(true);
