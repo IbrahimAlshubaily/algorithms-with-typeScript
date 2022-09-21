aff18340f775f2b02cd683c2a82f243816e3abbc
IbrahimAlshubaily <ibra.94@live.com>
2022-09-21 08:08:33 +0300

    Implement Stack (passed first try)

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,40 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private data: T[];
+    private capacity: number;
 
     constructor() {
+        this.length = 0;
+        this.capacity = 32;
+        this.data = Array(this.capacity);
     }
 
     push(item: T): void {
+        this.insureCapacity();
+        this.data[this.length] = item;
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (this.length === 0) {
+            return;
+        }
+        this.length--;
+        return this.data[this.length];
+    }
 
-}
     peek(): T | undefined {
+        if (this.length === 0) {
+            return;
+        }
+        return this.data[this.length-1];
+    }
 
-}
+    private insureCapacity(): void{
+        if(this.length < this.capacity) {
+            return;
+        }
+        this.data.concat(Array(this.capacity));
+        this.capacity *= 2;
+    }
 }
