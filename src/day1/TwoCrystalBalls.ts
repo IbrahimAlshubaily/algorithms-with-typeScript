9846bb79c54bb5d6c1d688bbe6de9c3a6c23e7eb
IbrahimAlshubaily <ibra.94@live.com>
2022-09-21 07:50:39 +0300

    solve TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const lenSqrt = Math.sqrt(breaks.length);
+
+    let curr = lenSqrt;
+    while (curr < breaks.length && !breaks[curr]) {
+        curr += lenSqrt;
+    }
+
+    curr -= lenSqrt;
+
+    for (let i = curr; i <= curr+lenSqrt; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
